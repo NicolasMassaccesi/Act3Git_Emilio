ac885511eb44cfd692284c124d209de013fbb70d
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-22 10:21:13 +0100

    Fusiono Rama2 con Rama1

--- E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
+++ E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
@@ -6,20 +6,29 @@ public class NumAleatorioApp {
     Scanner sc = new Scanner(System.in);
 
     System.out.print("Introduce el primer numero: ");
-    int numero1 = Integer.parseInt(sc.nextLine());
+      int numero1 = Integer.parseInt(sc.nextLine());
 
-    System.out.print("Introduce el primer numero: ");
-    int numero2 = Integer.parseInt(sc.nextLine());
+    System.out.print("Introduce el segundo numero: ");
+      int numero2 = Integer.parseInt(sc.nextLine());
+    
+    System.out.print("Introduce el tercer numero: ");
+      int numero3 = Integer.parseInt(sc.nextLine());
+    
 
-
+      if (numero3 > numero1 && numero3 < numero2) {
+        System.out.println("El numero " + numero3 + " se encuentra entre el " + numero1 + " y " + numero3);
+      }
+      
     if (numero1 >= numero2) {
      System.out.println("El primer numero debe ser menor que el segundo número");
     }
 
     while(numero1 <= numero2) {
       System.out.println(numero1);
-      numero1 += 7;
+      numero1 += 7; 
     }
+    
+    
     sc.close();
   }
 
