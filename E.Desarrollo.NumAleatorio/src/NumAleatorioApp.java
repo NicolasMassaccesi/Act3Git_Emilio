f7083f1295207f6822a8a845f9a9e79630857a30
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-22 09:23:46 +0100

    Primera Version

---
+++ E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class NumAleatorioApp {
+
+  public static void main(String[] args) {
+
+    Random generator = new Random();
+
+    for (int i = 1; i <= 20; i++) {
+      double randomNumber = generator.nextDouble(1.0, 10.0);
+      System.out.println("Este es el numero " + i + ": " + (int) randomNumber);
+    }
+
+  }
+
+}
