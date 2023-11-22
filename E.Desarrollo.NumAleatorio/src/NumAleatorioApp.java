e2b69e45d64b81e728618e0a6ab7e2fbda52a0ff
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-22 11:11:03 +0100

    Version V0

--- E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
+++ E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
@@ -1,34 +1,15 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class NumAleatorioApp {
 
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Random random = new Random();
 
-    System.out.print("Introduce el primer numero: ");
-      int numero1 = Integer.parseInt(sc.nextLine());
-
-    System.out.print("Introduce el segundo numero: ");
-      int numero2 = Integer.parseInt(sc.nextLine());
-    
-    System.out.print("Introduce el tercer numero: ");
-      int numero3 = Integer.parseInt(sc.nextLine());
-    
-
-      if (numero3 > numero1 && numero3 < numero2) {
-        System.out.println("El numero " + numero3 + " se encuentra entre el " + numero1 + " y " + numero3);
-      }
-      
-    if (numero1 >= numero2) {
-     System.out.println("El primer numero debe ser menor que el segundo número");
+    for (int i = 1; i <= 20; i++) {
+      double generator = random.nextDouble(1.0, 10.0);
+      System.out.println("El numero " + i + " es: " + (int) generator);
     }
 
-    while(numero1 <= numero2) {
-      System.out.println(numero1);
-      numero1 += 7; 
-    }
-    
-    
-    sc.close();
   }
-  }
+}
