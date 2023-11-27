d2e2e2482e60465563e3e1a36b30c558efedddd8
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-27 08:33:53 +0100

    Prueba para el merge

--- E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
+++ E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
@@ -10,15 +10,5 @@ public class NumAleatorioApp {
       System.out.println("El numero " + i + " es: " + (int) generator);
     }
 
-<<<<<<< HEAD
-=======
-    while(numero1 <= numero2) {
-      System.out.println(numero1);
-      numero1 += 8; 
-    }
-    
-    
-    sc.close();
->>>>>>> refs/heads/rama1
   }
 }
