1e82cd4009a3891230e5dff01b2ff4858d858c72
EmilioFdez12 <emiliofdezglld12@gmail.com>
2023-11-27 08:26:23 +0100

    Modifico el Programa para 30 num

--- E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
+++ E.Desarrollo.NumAleatorio/src/NumAleatorioApp.java
@@ -5,7 +5,7 @@ public class NumAleatorioApp {
   public static void main(String[] args) {
     Random random = new Random();
 
-    for (int i = 1; i <= 20; i++) {
+    for (int i = 1; i <= 30; i++) {
       double generator = random.nextDouble(1.0, 10.0);
       System.out.println("El numero " + i + " es: " + (int) generator);
     }
